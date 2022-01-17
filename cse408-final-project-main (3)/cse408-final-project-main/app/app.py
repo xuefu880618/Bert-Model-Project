ed37a0768d1d542ddd5b7696d71ff4fa67fda3e0
xuefu0618 <51739872+xuefugod@users.noreply.github.com>
2022-01-18 07:01:13 +0800

    Add files via upload

---
+++ cse408-final-project-main (3)/cse408-final-project-main/app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, jsonify
+from model import Predictor
+from unit_test import UnitTest
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('Home.html')
+
+
+@app.route('/article', methods=['GET'])
+def article():
+    return render_template('Article.html')
+
+
+@app.route('/analysis', methods=['GET'])
+def analysis(text):
+    # print('input', text)
+    return render_template('Analysis.html', article=text)
+
+
+@app.route("/post_submit", methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        res = request.form.get('text-to-detect')
+        # print(res)
+        res = Predictor(res)
+        res = res.predict()
+        return analysis(res)
+        # res = UnitTest()
+        # return analysis(res.sentence_output)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ cse408-final-project-main (3)/cse408-final-project-main/dataset/README.md
@@ -0,0 +1,5 @@
+# Dataset
+
+We gathered 20 privacy policies from famous companys in different fields.
+
+**merge.tsv** is the final result.
